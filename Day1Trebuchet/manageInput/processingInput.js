6e587864eb31d72f56c35f25f754b36f11b2c375
Dimitar Klunkoff <klunkoff@gmail.com>
2024-06-02 10:58:13 +0300

    create read input from file functionality

--- Day1Trebuchet/manageInput/processingInput.js
+++ Day1Trebuchet/manageInput/processingInput.js
@@ -1,14 +1,9 @@
-import { rawInput } from "./input.js";
-
-function refactoringInput(input) {
-    const inputArray = input.split('\n');
-    return inputArray;
-}
+import { getRawInput } from "./readInputFromFile.js";
 
 function getWorkingInput() {
     
-    const refactoredInput = refactoringInput(rawInput);
-    const stringsWithDigitsArray = refactoredInput.filter(string => string.match(/\d/g));
+    const rawInput = getRawInput();
+    const stringsWithDigitsArray = rawInput.filter(string => string.match(/\d/g));
     return stringsWithDigitsArray;
 }
 
