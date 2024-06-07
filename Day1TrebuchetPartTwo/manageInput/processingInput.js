5df8d591dd3220f25a37e7a3cd9ef5a62d707837
Dimitar Klunkoff <klunkoff@gmail.com>
2024-06-07 19:20:30 +0300

    Update processingInput.js

--- Day1TrebuchetPartTwo/manageInput/processingInput.js
+++ Day1TrebuchetPartTwo/manageInput/processingInput.js
@@ -1,10 +1,11 @@
 import { getRawInput } from "./readInputFromFile.js";
 
 function getWorkingInput() {
-    
+
     const regex = /(\d+|one|two|three|four|five|six|seven|eight|nine)/g;
     const rawInput = getRawInput();
     const stringsWithDigitsArray = rawInput.filter(string => string.match(regex));
+    console.log(stringsWithDigitsArray);
     return stringsWithDigitsArray;
 }
 
